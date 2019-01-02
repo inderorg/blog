e12a77409689efffcf2740398f093593f6b0aa3c
Inder Singh <inder.patliya@LM-ILB-INDER.local>
2019-01-02 18:35:08 +0530

    Updates

---
+++ precache-manifest.64ea2b3b8e4b1dc98265dcf463195af7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "255cd0f7ab465faad54a",
+    "url": "/blog/static/js/runtime~main.255cd0f7.js"
+  },
+  {
+    "revision": "34ba9ff21edc59e5a008",
+    "url": "/blog/static/js/main.34ba9ff2.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/blog/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "34ba9ff21edc59e5a008",
+    "url": "/blog/static/css/main.2c9cd321.chunk.css"
+  },
+  {
+    "revision": "93cac26b42dcc2e3b63316ec2d52a665",
+    "url": "/blog/index.html"
+  }
+];
